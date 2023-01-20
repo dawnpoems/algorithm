4e6041d954a38acaf7cde7f1b024d50656f20c3a
dawnpoem <108113517+dawnpoems@users.noreply.github.com>
2023-01-20 05:06:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+
+  //삼각형의 면적 구하기
+  //base4, height 2
+    
+  double base = 4;
+  double height = 2;
+
+  double area = base*height/2;
+
+  System.out.println(area);
+    
+  }
+}
